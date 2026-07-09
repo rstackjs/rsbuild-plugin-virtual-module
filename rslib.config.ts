bdf3eddda9bebe94c1d3dc426e01eb2b148276e6
Jiahan Chen <jait.chen@foxmail.com>
2026-07-09 17:57:12 +0800

    chore: update TypeScript to 7.0.2 (#37)

--- rslib.config.ts
+++ rslib.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@rslib/core';
 
 export default defineConfig({
   lib: [
-    { syntax: 'es2021', dts: { tsgo: true } },
+    { syntax: 'es2021', dts: true },
     { format: 'cjs', syntax: 'es2021' },
   ],
 });
